9ba1889b35f32088c378984f603d227e221ee003
Michael Lacanilao <michael.v.lacanilao@gmail.com>
2025-10-24 22:19:54 -0400

    Update version

--- MoreFishRarities/MoreFishRarities.cs
+++ MoreFishRarities/MoreFishRarities.cs
@@ -7,7 +7,7 @@ namespace MoreFishRarities
     {
         internal const string Guid = "omegaplatinum.elin.morefishrarities";
         internal const string Name = "More Fish Rarities";
-        internal const string Version = "1.1.1.1";
+        internal const string Version = "1.1.1.2";
     }
 
     [BepInPlugin(GUID: ModInfo.Guid, Name: ModInfo.Name, Version: ModInfo.Version)]
